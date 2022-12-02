c6b51195c7774164262ca1f49f478f8248c94a26
Techi-Joe <techi.jo3@gmail.com>
2022-12-01 23:51:17 -0500

    1.0

--- tgt_intv.py
+++ tgt_intv.py
@@ -1,18 +1,26 @@
-tgt_len = 1500
-#int(input("what is the target length (ft)? "))
-ord = 3
-#int(input("how much ordinance are you carrying? "))
-hspd = 500 # horizontal speed in ft/s
+tgt_len = float(input("what is the target length (nautical miles)? "))
+ord = int(input("how much ordinance are you carrying? "))
+hspd = (float(input("what is your speed (knots)? ")) /60)
 intvs = [0.06, 0.1, 0.14]
-comp_intvs = []
+comp_intvs = {}
 
 # note: for now all in ft
 
 tru_intv = tgt_len/hspd/ord
 
-for intv in intvs:
-    comp_intvs.append(tru_intv-intv)
-print(comp_intvs)
-print(tru_intv)
+# create a dictionary of all the values and compare them
+# dissallow numbers that comapre negatively
+# check if anything is put in dict at all
 
-# TODO: find loweset comp_intvs
+flag = False
+for intv in intvs:
+    if (tru_intv-intv >= 0):
+        comp_intvs[tru_intv-intv] = intv
+        flag = True
+if not flag:
+    comp_intvs = {0.00: min(intvs)}
+        
+print("\nperfect drop interval: " + str(tru_intv))
+print("best possible drop interval: " + str(comp_intvs[min(comp_intvs)]))
+if (comp_intvs[min(comp_intvs)] > tru_intv):
+    print("*note: not all ordinance can be used in one run-in")
